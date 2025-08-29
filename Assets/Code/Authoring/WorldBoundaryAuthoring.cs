58bc963b44e12e83014458c640b6a875d7be64cc
(ubuntu) Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2025-08-29 20:32:46 +0600

    Proper name

--- Assets/Code/Authoring/WorldBoundaryAuthoring.cs
+++ Assets/Code/Authoring/WorldBoundaryAuthoring.cs
@@ -5,14 +5,14 @@ namespace SimpleSetupEcs2d
 {
     public class WorldBoundaryAuthoring : MonoBehaviour
     {
-        public float interval = 1f;
+        public float updateInterval = 1f;
 
         public class WorldBoundaryBaker : Baker<WorldBoundaryAuthoring>
         {
             public override void Bake(WorldBoundaryAuthoring authoring)
             {
                 var entity = GetEntity(TransformUsageFlags.None);
-                AddComponent(entity, new WorldBoundary { updateInterval = authoring.interval });
+                AddComponent(entity, new WorldBoundary { updateInterval = authoring.updateInterval });
             }
         }
     }
